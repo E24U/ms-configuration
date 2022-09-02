989bbbc099a0d41a2b57d8ed7e671cac149f4fac
Merkurev Sergei <MerkurevSergei@yandex.ru>
2022-09-02 22:17:47 +0900

    Init Spring Boot

---
+++ src/test/java/ru/e24u/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,31 @@
+package ru.e24u.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@TestPropertySource(
+    properties = {
+        "spring.datasource.url = jdbc:tc:postgresql://localhost:5401/integration-tests-db",
+        "spring.datasource.driver-class-name = org.testcontainers.jdbc.ContainerDatabaseDriver",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect"
+    }
+)
+class ConfigurationApplicationTests {
+
+    @Container
+    private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer()
+        .withDatabaseName("foo")
+        .withUsername("foo")
+        .withPassword("secret");
+
+    @Test
+    void contextLoads() {
+    }
+
+}
